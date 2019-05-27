99417f3461562e6f1d2fa65558f029a411599614
Eden Shuker <edenshuker1997@gmail.com>
2019-05-27 15:35:23 +0300

    remove words exceed screen

--- src/ztype/game_controller.py
+++ src/ztype/game_controller.py
@@ -3,7 +3,7 @@ from pygame.locals import QUIT
 from level import Level
 from board import Board
 from config import FPS, SCREEN_SIZE, WHITE, LEVEL_WORD_COUNT, \
-    LEVEL_FREQUENCY, LEVEL_SPEED, SCREEN_BACKGROUND
+    LEVEL_FREQUENCY, LEVEL_SPEED, SCREEN_BACKGROUND, SCREEN_HEIGHT
 
 
 class GameController(object):
@@ -23,11 +23,16 @@ class GameController(object):
         self.screen = pygame.display.set_mode(SCREEN_SIZE)
         self.clock = pygame.time.Clock()
 
+    def remove_words_exceed_screen(self):
+        map(lambda word: word.remove(self.words_group),
+            filter(lambda word: word.rect.bottom > SCREEN_HEIGHT, self.words_group.sprites()))
+
     def run_one_frame(self):
         """
         Drop words down the screen.
         """
         self.words_group.update()
+        self.remove_words_exceed_screen()
 
     def run(self):
         """
